02caff0d5c2e01f832cc2c237329c7a1b8f1e88b
amnajamil <amnajamil445@gmail.com>
2025-09-05 15:33:20 +0500

    cloned and updated by amna

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello World Nimra Gul</Text>
+      <Text>Hello World ........it's amna here todayy hyeeee </Text>
       <StatusBar style="auto" />
     </View>
   );
